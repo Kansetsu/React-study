186bca76c464c67cec137dc87be4ed3fa915452a
Vinícius Andrade <vinicius802010@hotmail.com>
2022-04-11 13:28:59 -0300

    Added UseCallback examples
    Added new exercises, now about UseCallback.

--- hooks/src/views/examples/UseCallback.tsx
+++ hooks/src/views/examples/UseCallback.tsx
@@ -1,12 +1,23 @@
+import { useCallback, useState } from 'react'
 import PageTitle from '../../components/layout/PageTitle'
+import UseCallbackButtons from './UseCallbackButtons'
 
 const UseCallback = (props: any) => {
+    const [count, setCount] = useState(0)
+
+    const inc = useCallback(function (delta: number) {
+        setCount(curr => curr + delta)
+    }, [setCount])
+
     return (
         <div className="UseCallback">
-            <PageTitle
-                title="Hook UseCallback"
-                subtitle="Retorna uma função memoizada!"
-            />
+            <PageTitle title="Hook UseCallback" subtitle="Retorna uma função memoizada!" />
+
+            <div className="center">
+                <span className="text">{count}</span>
+                <UseCallbackButtons inc={inc} />
+
+            </div>
         </div>
     )
 }
